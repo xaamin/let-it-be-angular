48923c72f64dada86038236de7b05ff25d67ac8d
Benjamín Martínez Mateos <xaamin@outlook.com>
2022-04-02 19:31:23 -0600

    Allowing multiple service calling using the @ approach (Magic)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
   production: true,
+
+  // I really dislike this, I prefer to use dotenv (https://www.npmjs.com/package/dotenv)
+  // to avoid putting sensitive data into versioned files (git)
+  servers: {
+    '@api': 'https://api.itnovado.com',
+    '@media': 'https://media.itnovado.com',
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,10 @@
 export const environment = {
   production: false,
+
+  // I really dislike this, I prefer to use dotenv (https://www.npmjs.com/package/dotenv)
+  // to avoid putting sensitive data into versioned files (git)
+  servers: {
+    '@api': 'https://fakestoreapi.com',
+    '@media': 'https://jsonplaceholder.com',
+  }
 };
